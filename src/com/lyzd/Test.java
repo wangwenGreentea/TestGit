c82a49f6923ff3195070ebf1b65964fc4cc5bbb6
wangwen <1747764238@qq.com>
2020-09-30 11:11:48 +0800

    Initial commit

---
+++ src/com/lyzd/Test.java
@@ -0,0 +1,11 @@
+package com.lyzd;
+
+/**
+ * @author wangwen
+ * @create 2020-09-30 11:05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
